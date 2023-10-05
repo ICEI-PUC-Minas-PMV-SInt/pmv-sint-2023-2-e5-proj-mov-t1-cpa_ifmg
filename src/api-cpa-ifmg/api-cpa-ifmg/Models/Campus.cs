71f6a1027a07b6c05c24a2ae31b5e013c8095619
dridrileite <adrianacristinasouza@gmail.com>
2023-10-05 08:51:36 -0300

    criação da classe campus

---
+++ src/api-cpa-ifmg/api-cpa-ifmg/Models/Campus.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_cpa_ifmg.Models
+{
+    public class Campus
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+    }
+}
