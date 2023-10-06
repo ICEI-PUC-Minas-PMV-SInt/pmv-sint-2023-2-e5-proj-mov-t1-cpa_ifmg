b8732232c1e91abfe4a63158d5458420b1c60d09
dridrileite <adrianacristinasouza@gmail.com>
2023-10-06 08:14:01 -0300

    definição da classe proposta

--- src/api-cpa-ifmg/api-cpa-ifmg/Models/Campus.cs
+++ src/api-cpa-ifmg/api-cpa-ifmg/Models/Campus.cs
@@ -1,12 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api_cpa_ifmg.Models
 {
+    [Table("Campus")]
     public class Campus
     {
         [Key]
         public int id { get; set; }
         [Required]
         public string name { get; set; }
+        [Required]
+        public int PropostaId { get; set; }
+        public Proposta Proposta { get; set; }
     }
 }
---
+++ src/api-cpa-ifmg/api-cpa-ifmg/Models/Proposta.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_cpa_ifmg.Models
+{
+    [Table("Proposta")]
+    public class Proposta
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public TipoProposta Tipo { get; set; }
+        [Required]
+        public SituacaoProposta Situacao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+
+        public ICollection<Campus> Unidade { get; set; }
+        
+     
+
+    }
+    public enum TipoProposta
+    {
+        Cadastro_Membros,
+        Relatorio
+    }
+    public enum SituacaoProposta
+    {
+        Elaboracao,
+        Efetivada,
+        Enviada
+    }
+}
